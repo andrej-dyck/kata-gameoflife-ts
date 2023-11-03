cf64e33ef84d77c45937751d09f740ed73ab5633
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2023-11-03 16:11:59 +0100

    ✔ add further conway's gol patterns

--- src/game-of-life.test.ts
+++ src/game-of-life.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from 'vitest'
 import { simulate } from './game-of-life.ts'
 import { conwaysOriginalRule } from './evolution-rule.ts'
-import { Board, coordinate, infinite2dBoard } from './board.ts'
+import { Board, Coordinate, coordinate, infinite2dBoard } from './board.ts'
 import { Set } from 'immutable'
 import { omitFunctions, take } from './ts-extensions.ts'
 
@@ -146,14 +146,148 @@ describe('oscillating patterns', () => {
   })
 })
 
+describe('stable patterns', () => {
+
+  test('small rotated-L', () => {
+    const seed = testBoard(
+      '.##.',
+      '.#..',
+    )
+    const stable = testBoard(
+      '.##.',
+      '.##.',
+    )
+    expectBoardSequence(
+      seed,
+      stable,
+      stable
+    )
+  })
+  test('large rotated-L', () => {
+    const seed = testBoard(
+      '.....',
+      '.###.',
+      '.#...',
+      '.....',
+    )
+    const stable = testBoard(
+      '.##..',
+      '#..#.',
+      '.##..',
+      '.....',
+    )
+    expectBoardSequence(
+      seed,
+      testBoard(
+        '..#..',
+        '.##..',
+        '.#...',
+        '.....',
+      ),
+      testBoard(
+        '.##..',
+        '.##..',
+        '.##..',
+        '.....',
+      ),
+      stable,
+      stable
+    )
+  })
+  test('large I', () => {
+    const seed = testBoard(
+      '...#...',
+      '...#...',
+      '...#...',
+      '...#...',
+    )
+    const stable = testBoard(
+      '...#...',
+      '..#.#..',
+      '..#.#..',
+      '...#...',
+    )
+    expectBoardSequence(
+      seed,
+      testBoard(
+        '.......',
+        '..###..',
+        '..###..',
+        '.......',
+      ),
+      stable,
+      stable
+    )
+  })
+})
+
+describe('dying patterns', () => {
+
+  test('i', () => expectBoardSequence(
+    testBoard(
+      '..#.',
+      '.#..',
+      '.#..',
+    ),
+    testBoard(
+      '....',
+      '.##.',
+      '....',
+    ),
+    emptyBoard
+  ))
+  test('diagonal line', () => expectBoardSequence(
+    testBoard(
+      '...#.',
+      '..#..',
+      '.#...',
+    ),
+    testBoard(
+      '.....',
+      '..#..',
+      '.....',
+    ),
+    emptyBoard
+  ))
+  test('rotated-i', () => expectBoardSequence(
+    testBoard(
+      '......',
+      '.#....',
+      '..###.',
+      '......',
+    ),
+    testBoard(
+      '......',
+      '..##..',
+      '..##..',
+      '...#..',
+    ),
+    testBoard(
+      '......',
+      '..##..',
+      '....#.',
+      '..##..',
+    ),
+    testBoard(
+      '......',
+      '...#..',
+      '....#.',
+      '...#..',
+    ),
+    testBoard(
+      '......',
+      '......',
+      '...##.',
+      '......',
+    ),
+    emptyBoard
+  ))
+})
+
 describe('still-life', () => {
 
   test('empty board', () => expectNotChanging(
-    testBoard(
-      '...',
-      '...',
-      '...',
-    )
+    emptyBoard
   ))
   test('square', () => expectNotChanging(
     testBoard(
@@ -208,7 +342,9 @@ const testBoard = (...rows: string[]): Board => infinite2dBoard({
     rows.flatMap((row, rowIndex) =>
       [...row].map((c, colIndex) => [c, colIndex] as const)
         .filter(([c]) => c === '#')
-        .map(([c, colIndex]) => coordinate({ x: rowIndex, y: colIndex }))
+        .map(([_, colIndex]) => coordinate({ x: rowIndex, y: colIndex }))
     )
   )
 })
+
+const emptyBoard = infinite2dBoard({ livingCells: Set<Coordinate>() })
